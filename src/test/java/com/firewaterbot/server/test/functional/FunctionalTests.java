2c0840f1a0e09fd04deba77780c928afe60252c3
Vignesh <vignesh.p.kannan@gmail.com>
2016-08-03 22:20:06 -0500

    Bug fixes for surefire plugin and refactored test classes

--- src/test/java/com/firewaterbot/server/test/functional/FunctionalTests.java
+++ src/test/java/com/firewaterbot/server/test/functional/FunctionalTests.java
@@ -1,9 +1,9 @@
-package com.firewaterbot.server;
+package com.firewaterbot.server.test.functional;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @ContextConfiguration(locations = { "classpath:spring-test-config.xml" })
-public class AppTest extends AbstractTestNGSpringContextTests {
+public class FunctionalTests extends AbstractTestNGSpringContextTests {
 
 }
